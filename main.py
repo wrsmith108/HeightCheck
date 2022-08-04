b3422930007e393be8365fc29c890458ac989a5e
Ryan Smith <62075823+wrsmith108@users.noreply.github.com>
2022-08-04 21:58:11 +0000

    if else statement

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if height <= 120:
+  print("you can't ride the rollercoaster")
+else: 
+  print("welcome, enjoy your ride!")
